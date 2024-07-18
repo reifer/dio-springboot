16238dc7545dddefbf03eccd719e3e53c09ff305
reifer <reinaldofsjunior@gmail.com>
2024-07-18 01:17:03 -0300

    Beans vs Components

---
+++ src/main/java/dio/spring_beans_autowired/app/Beans.java
@@ -0,0 +1,13 @@
+package dio.spring_beans_autowired.app;
+
+import com.google.gson.Gson;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Beans {
+    @Bean
+    public Gson gson(){
+        return new Gson();
+    }
+}
